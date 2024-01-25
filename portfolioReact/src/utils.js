72caa512884c17cb99299aeb69e30eb4e03ce8a1
Martin-Cifre <martin.cifre@outlook.com>
2024-01-24 21:35:14 -0300

    ruta de utils arreglada

--- portfolioReact/src/utils.js
+++ portfolioReact/src/utils.js
@@ -1,3 +1,9 @@
 export const getImgUrl = (path) => {
+  return `/assets/${path}`;
+};
+
+/*
+export const getImgUrl = (path) => {
     return new URL(`/assets/${path}`, import.meta.url).href;
 }
+*/
