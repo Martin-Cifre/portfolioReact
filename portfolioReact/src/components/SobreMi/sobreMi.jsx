fe29c2eaf9ce708d09c5fb83e5695b29f31073d3
Martin-Cifre <martin.cifre@outlook.com>
2024-01-26 18:55:43 -0300

    correcciones sobre mi

--- portfolioReact/src/components/SobreMi/sobreMi.jsx
+++ portfolioReact/src/components/SobreMi/sobreMi.jsx
@@ -4,58 +4,59 @@ import styles from "./sobreMi.module.css";
 
 export const SobreMi = () => {
   return (
-    <section className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.sobre}>Sobre Mi</h1>
+    <section>
+      <h2>Sobre mi</h2>
+      <div>
         <img
           src={getImgUrl("SobreMi/pixarMe.png")}
-          alt="ilustracion"
+          alt="Martin Cifre version Pixar"
           className={styles.SobreImg}
         />
-      </div>
-      <div>
-        <div className={styles.items}>
-          <img
-            src={getImgUrl("SobreMi/front.png")}
-            alt="front"
-            className={styles.itemsImg}
-          />
-          <div>
-            <h3 className={styles.itemsTitle}>Front</h3>
-            <p className={styles.itemsTxt}>
-              Conocimientos en React capaz de crear sitios completamente responsive.
-            </p>
-          </div>
-        </div>
+        <ul>
+          <li>
+            <img
+              src={getImgUrl("SobreMi/front.png")}
+              alt="frontend"
+              className={styles.itemsImg}
+            />
+            <div>
+              <h3>Frontend</h3>
+              <p>
+                Conocimientos en React capaz de crear sitios completamente
+                responsive.
+              </p>
+            </div>
+          </li>
 
-        <div className={styles.items}>
-          <img
-            src={getImgUrl("SobreMi/back.png")}
-            alt="back"
-            className={styles.itemsImg}
-          />
-          <div>
-            <h3 className={styles.itemsTitle}>Back</h3>
-            <p className={styles.itemsTxt}>
-              Tengo conocimientos en React, base de datos con mySQL y APIs.
-            </p>
-          </div>
-        </div>
+          <li>
+            <img
+              src={getImgUrl("SobreMi/back.png")}
+              alt="frontend"
+              className={styles.itemsImg}
+            />
+            <div>
+              <h3>Backend</h3>
+              <p>
+                Tengo conocimientos en React, base de datos con mySQL y APIs.
+              </p>
+            </div>
+          </li>
 
-        <div className={styles.items}>
-          <img
-            src={getImgUrl("SobreMi/design.png")}
-            alt="diseño"
-            className={styles.itemsImg}
-          />
-          <div>
-            <h3 className={styles.itemsTitle}>Diseño Grafico</h3>
-            <p className={styles.itemsTxt}>
-              Experiencia profesional en diseño grafico habiendo trabajado para
-              Editorial Ivrea.
-            </p>
-          </div>
-        </div>
+          <li>
+            <img
+              src={getImgUrl("SobreMi/design.png")}
+              alt="Diseño"
+              className={styles.itemsImg}
+            />
+            <div>
+              <h3>Diseño Grafico</h3>
+              <p>
+                Experiencia profesional en diseño grafico habiendo trabajado
+                para Editorial Ivrea.
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
