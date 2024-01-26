52a7fba18c18efc7dcf0eb92a7ac5f72a8ddd3a8
Martin-Cifre <martin.cifre@outlook.com>
2024-01-26 19:52:01 -0300

    correcciones de css y hovers agregados

--- portfolioReact/src/components/SobreMi/sobreMi.jsx
+++ portfolioReact/src/components/SobreMi/sobreMi.jsx
@@ -4,22 +4,22 @@ import styles from "./sobreMi.module.css";
 
 export const SobreMi = () => {
   return (
-    <section>
-      <h2>Sobre mi</h2>
-      <div>
+    <section className={styles.container}>
+      <h2 className={styles.title}>Sobre mi</h2>
+      <div className={styles.content}>
         <img
           src={getImgUrl("SobreMi/pixarMe.png")}
           alt="Martin Cifre version Pixar"
           className={styles.SobreImg}
         />
-        <ul>
-          <li>
+        <ul className={styles.items}>
+          <li className={styles.item}>
             <img
               src={getImgUrl("SobreMi/front.png")}
               alt="frontend"
               className={styles.itemsImg}
             />
-            <div>
+            <div className={styles.itemsTxt}>
               <h3>Frontend</h3>
               <p>
                 Conocimientos en React capaz de crear sitios completamente
@@ -28,27 +28,27 @@ export const SobreMi = () => {
             </div>
           </li>
 
-          <li>
+          <li className={styles.item}>
             <img
               src={getImgUrl("SobreMi/back.png")}
               alt="frontend"
               className={styles.itemsImg}
             />
-            <div>
+            <div className={styles.itemsTxt}>
               <h3>Backend</h3>
               <p>
-                Tengo conocimientos en React, base de datos con mySQL y APIs.
+                Tengo conocimientos en Express, base de datos con mySQL y APIs.
               </p>
             </div>
           </li>
 
-          <li>
+          <li className={styles.item}>
             <img
               src={getImgUrl("SobreMi/design.png")}
               alt="Diseño"
               className={styles.itemsImg}
             />
-            <div>
+            <div className={styles.itemsTxt}>
               <h3>Diseño Grafico</h3>
               <p>
                 Experiencia profesional en diseño grafico habiendo trabajado
