dd7b1f1c4f02b41f4fb835c93b14c4be0af4dc17
Martin-Cifre <martin.cifre@outlook.com>
2024-01-27 14:06:54 -0300

    proyectos con css y responsive

--- portfolioReact/src/components/proyectos/proyectos.jsx
+++ portfolioReact/src/components/proyectos/proyectos.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import { getImgUrl } from "../../utils";
 import projects from "../../data/projects.json";
-//import styles from "./proyectos.module.css";
+import styles from "./proyectos.module.css";
 
 export const Proyectos = () => {
   return (
-    <section>
-      <h2>Proyectos</h2>
-      <div>
+    <section className={styles.container}>
+      <h2 className={styles.title}>Proyectos</h2>
+      <div className={styles.projects}>
         {projects.map((projects, id) => {
           return (
-            <div key={id}>
+            <div className={styles.cardContainer} key={id}>
               <img
+                className={styles.img}
                 src={getImgUrl(projects.imgSrc)}
                 alt={`imagen de ${projects.title}`}
               />
-              <h3>{projects.title}</h3>
-              <p>{projects.description} </p>
-              <ul>
+              <h3 className={styles.cardTitle}>{projects.title}</h3>
+              <p className={styles.description}>{projects.description} </p>
+              <ul className={styles.skills}>
                 {projects.skills.map((skill, id) => {
-                  <li key={id}>{skill}</li>;
+                  return(
+                  <li className={styles.skill} key={id}>
+                    {skill}
+                  </li>);
                 })}
               </ul>
 
-              <div>
-                <a href={projects.link}>Link</a>
-                <a href={projects.repositorio}>Repositorio</a>
+              <div className={styles.links}>
+                <a className={styles.link} href={projects.link}>
+                  Link
+                </a>
+                <a className={styles.link} href={projects.repositorio}>
+                  Repositorio
+                </a>
               </div>
             </div>
           );
